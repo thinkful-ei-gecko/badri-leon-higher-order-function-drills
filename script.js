68b3093c90f1cd8f273f76bf7bc65fca7c8b7b8c
Leon Dail <leondailofficial@gmail.com>
2019-08-13 12:15:30 -0700

    warning hazard system

--- script.js
+++ script.js
@@ -30,12 +30,41 @@ function filter(arr, fn) {
 
 // DO NOT EDIT BETWEEN THESE LINES, BUT DO READ THE CODE ----->
 // First we setup an array of strings we plan to filter:
-const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+//const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
 
 // We use your `filter` function here, capturing a new array into `filteredNames`
 // comprised of names that only begin with 'R'
-const filteredNames = filter(myNames, (name) => name[0] === 'R');
+//const filteredNames = filter(myNames, (name) => name[0] === 'R');
 
 //console.log(filteredNames) // => ['Rich', 'Ray']
 
 // <---- DO NOT EDIT BETWEEN THESE LINES
+
+
+function hazardWarningCreator(typeOfWarning) {
+  let warningCounter = 0;
+
+  return function(location) {
+    warningCounter++;
+    console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`);
+    if (warningCounter === 1) {
+      console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time today!`);
+    }
+    else {
+      console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} times today!`);
+    }
+  }
+}
+
+const rocksWarning = hazardWarningCreator('Rocks on the Road');
+const floodWarning = hazardWarningCreator('Water everywhere guys!!!!');
+const fireWarning = hazardWarningCreator('It\'s hot ya\'ll');
+rocksWarning('Pacific Ocean');
+rocksWarning('Los Angeles');
+floodWarning('Michigan');
+floodWarning('Carlsbad');
+floodWarning('North Carolina');
+/*fireWarning('It\'s REALLY hot ya\'ll');
+fireWarning('It\'s LIKE DANG HOT hot ya\'ll');
+fireWarning('It\'s  -what are you doing outside- hot ya\'ll');
+*/
